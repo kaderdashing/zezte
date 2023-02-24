8c94f396b109d99f3382a1e37b08b34e1e11fa13
kaderdashing <92338093+kaderdashing@users.noreply.github.com>
2023-02-24 10:31:15 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,16 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-  resolve: async (name) => {
-    const pages = import.meta.glob('./Pages/**/*.vue')
-
-    return (await pages[`./Pages/${name}.vue`]())
-  },
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
-  },
+    resolve: async(name) => {
+        // la function async va charger la totalité des pages (glob)
+        const pages =
+            import.meta.glob('./Pages/**/*.vue')
+            // la await method va attendre que tt les pages seront charger pour retourner la vue
+        return (await pages[`./Pages/${name}.vue`]())
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
 })
